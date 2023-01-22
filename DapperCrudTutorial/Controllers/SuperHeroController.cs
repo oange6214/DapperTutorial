d99d47df9edfa27b2f356491686646f2e5fa7afd
PoHan <oange6214@gmail.com>
2023-01-22 22:41:51 +0800

    Update 2

--- DapperCrudTutorial/Controllers/SuperHeroController.cs
+++ DapperCrudTutorial/Controllers/SuperHeroController.cs
@@ -16,10 +16,6 @@ namespace DapperCrudTutorial.Controllers
 			_config = config;
 		}
 
-		/// <summary>
-		/// 取得所有 Super Hero
-		/// </summary>
-		/// <returns></returns>
 		[HttpGet]
 		public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeros()
 		{
@@ -50,10 +46,27 @@ namespace DapperCrudTutorial.Controllers
 		public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero hero)
 		{
 			using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-			await connection.ExecuteAsync("");
-			return Ok(hero);
+			await connection.ExecuteAsync("INSERT INTO SuperHeros (name, firstname, lastname, place) values (@Name, @FirstName, @LastName, @Place)", hero);
+			return Ok(await SelectAllHeroes(connection));
 		}
-		private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(SqlConnection connection)
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("UPDATE SuperHeros SET name = @name, firstname = @FirstName, lastname = @LastName, place = @Place WHERE id = @Id", hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        [HttpDelete("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("DELETE FROM SuperHeros WHERE id = @Id", new { Id = heroId });
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(SqlConnection connection)
 		{
 			return await connection.QueryAsync<SuperHero>("SELECT * FROM SuperHeros");
 		}
